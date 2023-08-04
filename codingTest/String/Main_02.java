70ecd54df5d5e63c49beca6086f04eee879caffc
SEOJIN <kitty7579@hanmail.net>
2023-08-05 00:47:17 +0900

    02 대소문자 변환

---
+++ codingTest/String/Main_02.java
@@ -0,0 +1,26 @@
+package codingTest.String;
+
+import java.util.Scanner;
+
+/*
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+ */
+public class Main_02 {
+    public String solution(String str){
+        String answer = "";
+        for(char x : str.toCharArray()){
+            if(Character.isLowerCase(x)){
+                answer += Character.toUpperCase(x);
+            }else{
+                answer += Character.toLowerCase(x);
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main_02 T = new Main_02();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
