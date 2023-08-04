ef71885a73100e40b600410fe0954b693737adaf
SEOJIN <kitty7579@hanmail.net>
2023-08-05 00:59:27 +0900

    02 대소문자 변환(ascii code)

--- codingTest/String/Main_02.java
+++ codingTest/String/Main_02.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 
 /*
 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+ ASCII code : 소문자 97 ~ 122, 대문자 65 ~ 90
  */
 public class Main_02 {
     public String solution(String str){
         String answer = "";
         for(char x : str.toCharArray()){
-            if(Character.isLowerCase(x)){
-                answer += Character.toUpperCase(x);
-            }else{
-                answer += Character.toLowerCase(x);
+            if(x >= 97 && x <= 122) { // 소문자
+                answer += (char)(x-32);
+            }else {
+                answer += (char)(x+32);
             }
+//            if(Character.isLowerCase(x)){
+//                answer += Character.toUpperCase(x);
+//            }else{
+//                answer += Character.toLowerCase(x);
+//            }
         }
         return answer;
     }
