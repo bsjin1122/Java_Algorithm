692a9caf93b554b706814c1fc142128c5e60962f
SEOJIN <kitty7579@hanmail.net>
2023-12-05 23:56:30 +0900

    백준 스택-9093 문제 풀이중(231205)

---
+++ TestProgrammers/Baekjoon/Boj_9093.java
@@ -0,0 +1,23 @@
+package TestProgrammers.Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj_9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        // Stack 객체 생성.
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i < n; i++){
+
+        }
+
+
+
+    }
+}
